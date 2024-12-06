76deaea7908ba2fafb8d5ecf01319413abca422a
Juan Pablo Roda <pivolo@gmail.com>
2024-12-06 09:40:25 +0100

    feat: add it test for getting products

---
+++ src/test/java/com/mycomp/products/infrastructure/inbound/rest/v1/ProductsControllerIT.java
@@ -0,0 +1,42 @@
+package com.mycomp.products.infrastructure.inbound.rest.v1;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@WebMvcTest
+class ProductsControllerIT {
+    @Autowired
+    private MockMvc mvc;
+
+
+    @Test
+    void getProductsWithoutFiltering() throws Exception {
+        mvc.perform(MockMvcRequestBuilders
+                        .get("/products")
+                        .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(1)))
+                .andExpect(jsonPath("$[0].sku", is("sku1")));
+    }
+
+}
